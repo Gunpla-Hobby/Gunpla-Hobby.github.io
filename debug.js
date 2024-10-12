ce33704cfc46df22bb2344056d6a2bb7c7c235e9
XiangPingJun <XiangPingJun@users.noreply.github.com>
2024-10-13 01:25:43 +0800

    .

--- debug.js
+++ debug.js
@@ -11,16 +11,25 @@ import {
   update,
 } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js"
 
+
 if (window.location.pathname.startsWith('/debug')) {
-  const apiKey = /debug\/(.+)/g.exec(window.location.pathname)[1]
-  console.log(apiKey)
-  const app = initializeApp({
-    apiKey,
+  document.getElementById('particleCanvas').style.display = 'none'
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAPwxFKKRVV8WNtNEGxdYtStHwe6A4cBdA",
     authDomain: "gunpla-hobby.firebaseapp.com",
     databaseURL: "https://gunpla-hobby-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "gunpla-hobby",
     storageBucket: "gunpla-hobby.appspot.com",
     messagingSenderId: "165696452489",
     appId: "1:165696452489:web:6fe3240a62637d1b9c9358"
-  })
+  };
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+
+  const appCheck = firebase.appCheck();
+  appCheck.activate('6Ldhml8qAAAAALT_6VS1bXdqZn_CIMozlZShYSC4', true);
+
+  firebase.database().ref('initCtrl').set(JSON.stringify(controls.getCameraParam()))
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as SPLAT from "./gsplat.js"
+import * as SPLAT from "/gsplat.js"
 
 const canvas = document.getElementById("splatCanvas")
 const renderer = new SPLAT.WebGLRenderer(canvas)
@@ -38,7 +38,7 @@ canvas.addEventListener('touchstart', () => controls.autoRotate = 0)
 
 async function loadModel({ filename, initCtrl }) {
   await SPLAT.Loader.LoadAsync(
-    `splatData/${filename}.splat`,
+    `/splatData/${filename}.splat`,
     scene,
     (progress) => {
       // console.log(progress)
@@ -70,29 +70,3 @@ loadDataItem(window.data[0])
 
 window.addEventListener('resize', checkIfHorizontal)
 checkIfHorizontal()
-
-/*
-if (sessionStorage.debug) {
-  document.getElementById('particleCanvas').style.display = 'none'
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyAPwxFKKRVV8WNtNEGxdYtStHwe6A4cBdA",
-    authDomain: "gunpla-hobby.firebaseapp.com",
-    databaseURL: "https://gunpla-hobby-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "gunpla-hobby",
-    storageBucket: "gunpla-hobby.appspot.com",
-    messagingSenderId: "165696452489",
-    appId: "1:165696452489:web:6fe3240a62637d1b9c9358"
-  };
-
-  const app = firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
-
-  const appCheck = firebase.appCheck();
-  appCheck.activate('YOUR_RECAPTCHA_SITE_KEY', true);
-
-  setTimeout(() => {
-    JSON.stringify(controls.getCameraParam())
-  }, [1000])
-}
-*/
