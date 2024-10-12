a096d15e823d6af6c8e02aea5939483a7f658b06
XiangPingJun <XiangPingJun@users.noreply.github.com>
2024-10-13 01:55:18 +0800

    .

--- debug.js
+++ debug.js
@@ -9,31 +9,35 @@ import {
   push,
   remove,
   update,
-} from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js"
+} from "//www.gstatic.com/firebasejs/9.9.0/firebase-database.js"
+import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app-check.js"
 
 
-console.log(window.location.pathname)
-if (window.location.pathname.startsWith('/debug')) {
+//if (window.location.pathname.startsWith('/debug')) {
 
-  document.getElementById('particleCanvas').style.display = 'none'
+document.getElementById('particleCanvas').style.display = 'none'
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAPwxFKKRVV8WNtNEGxdYtStHwe6A4cBdA",
-    authDomain: "gunpla-hobby.firebaseapp.com",
-    databaseURL: "https://gunpla-hobby-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "gunpla-hobby",
-    storageBucket: "gunpla-hobby.appspot.com",
-    messagingSenderId: "165696452489",
-    appId: "1:165696452489:web:6fe3240a62637d1b9c9358"
-  };
+const app = initializeApp({
+  apiKey: "AIzaSyAPwxFKKRVV8WNtNEGxdYtStHwe6A4cBdA",
+  authDomain: "gunpla-hobby.firebaseapp.com",
+  databaseURL: "https://gunpla-hobby-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gunpla-hobby",
+  storageBucket: "gunpla-hobby.appspot.com",
+  messagingSenderId: "165696452489",
+  appId: "1:165696452489:web:6fe3240a62637d1b9c9358"
+})
 
-  const app = firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6Ldhml8qAAAAALT_6VS1bXdqZn_CIMozlZShYSC4'),
+  isTokenAutoRefreshEnabled: true
+})
 
-  const appCheck = firebase.appCheck();
-  appCheck.activate('6Ldhml8qAAAAALT_6VS1bXdqZn_CIMozlZShYSC4', true);
+const database = getDatabase(app)
 
-  setTimeout(() => {
-    firebase.database().ref('initCtrl').set(JSON.stringify(controls.getCameraParam()))
-  }, [1000])
-}
+console.log(database)
+
+setTimeout(() => {
+  database.ref('initCtrl').set(JSON.stringify(controls.getCameraParam()))
+}, [1000])
+
+//}
